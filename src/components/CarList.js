fdbcaa9c5bd297dd2e877da245ea3d604051b475
konojack <konojack@gmail.com>
2023-01-13 18:07:02 +0100

    bold listed names

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,12 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { removeCar } from '../store';
 
 const CarList = () => {
-  const cars = useSelector(({ cars: { searchTerm, data } }) =>
-    data.filter(
+  const { cars, name } = useSelector(({ form, cars: { searchTerm, data } }) => {
+    const filteredCars = data.filter(
       car =>
         car.name.toLowerCase().includes(searchTerm.toLowerCase()) || !searchTerm
-    )
-  );
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
+  });
 
   const dispatch = useDispatch();
 
@@ -16,21 +21,27 @@ const CarList = () => {
     dispatch(removeCar(carId));
   };
 
+  const renderedCars = cars?.map(car => {
+    const isBold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div key={car.id} className={`panel ${isBold && 'bold'}`}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
   return (
     <div className="car-list">
-      {cars?.map(car => (
-        <div key={car.id} className="panel">
-          <p>
-            {car.name} - ${car.cost}
-          </p>
-          <button
-            className="button is-danger"
-            onClick={() => handleCarDelete(car.id)}
-          >
-            Delete
-          </button>
-        </div>
-      ))}
+      {renderedCars}
       <hr />
     </div>
   );
